cc3adf487758f37383110dad1abffbce92842480
Ameya500 <ameya.achalla@gmail.com>
2026-02-22 03:06:07 +0530

    backup

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,6 @@ export const ENV = {
     // - Android emulator: 10.0.2.2
     // - iOS simulator: 127.0.0.1 (or localhost)
     // - Physical device: use your machine's local IP (e.g., 192.168.x.x)
-    API_URL: process.env.EXPO_PUBLIC_API_URL || 'http://10.0.2.2:8080/api/v1',
+    API_URL: process.env.EXPO_PUBLIC_API_URL || 'http://10.33.122.128:8000',
     IS_DEV: process.env.NODE_ENV === 'development',
 };
